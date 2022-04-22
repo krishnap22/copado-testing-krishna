84559e80d15c77186a16893c1eeb18f279458aa5
Krishna Prasad Veluthakkal <kveluthakkal@copado.com>
2022-04-22 08:06:07 +0000

    US-0000390: Test story

--- lwc/lwctest/lwctest.js
+++ lwc/lwctest/lwctest.js
@@ -2,7 +2,7 @@ import { LightningElement ,track} from 'lwc';
 
 export default class Lwctest extends LightningElement {
 
-    @track greeting = 'World';
+    @track greeting = 'World 2';
     changeHandler(event) {
         this.greeting = event.target.value;
     }
